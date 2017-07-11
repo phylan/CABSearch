a4bb6f8a84487d95c9c89790234cd08bfcf362e6
Ryan Sommers <ryansommers@gmail.com>
2017-07-11 17:30:56 -0400

    Fix mini-server errors

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ const forceSSL = function() {
 	}
 }
 
+app.use(express.static(__dirname + '/dist'));
+app.use(forceSSL());	
+
+
 app.get('/*', function(req,res) {
 	res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-app.use(forceSSL());	
-app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
